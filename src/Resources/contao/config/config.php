b6876176cdc3d20922702f17554a09181bcd4762
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-05-19 14:34:40 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -12,3 +12,18 @@ array(
     )
 )
 );
+
+if ('BE' === TL_MODE) {
+    
+    $GLOBALS['TL_CSS'][] = '/bundles/promotionmodal/css/lb_be_icon.css';
+
+    
+    
+}
+if(\Input::get('do') == 'lb_promo'&&\Input::get('id'))
+{
+    $GLOBALS['TL_CSS'][] = '/bundles/promotionmodal/css/lb_be_promo.css';
+    
+}
+
+$GLOBALS['TL_HOOKS']['parseFrontendTemplate'][] = array(\LocalbrandingDe\PromotionModalBundle\EventListener\PromoFrontendTemplateListener::class, 'lbPromoParseFrontendTemplate');
