0e21d4b1c64c33127a79fc980287152b9a245ad7
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-05-19 14:36:20 +0200

    Update newsletterpopup.js

--- src/Resources/public/js/newsletterpopup.js
+++ src/Resources/public/js/newsletterpopup.js
@@ -4,6 +4,52 @@ var idletime = 119;
 var callTime =30;
 
 
+var modalTrigger;
+function triggerModal(id) {
+	if(modalTrigger[id])
+	{
+		MicroModal.close('modal-'+id);
+		modalTrigger[id]=false;
+	}
+	else{
+		MicroModal.show('modal-'+id);
+		modalTrigger[id]=true;
+		
+	}
+	
+	
+}
+jQuery('document').ready(function(){
+	
+	
+
+	TimeMe.initialize({
+		idleTimeoutInSeconds: idletime
+		
+	});
+	jQuery('.micromodal-slide').each(function(){
+		var id = jQuery(this).attr('id').split("-").pop();
+		var pages =jQuery(this).attr('pages');
+		var start =jQuery(this).attr('start');
+		  var stop =jQuery(this).attr('stop');
+		  var bestBefore =jQuery(this).attr('bestBefore');
+		  var duration =jQuery(this).attr('duration');
+		if(pages=="true")
+			{
+			popNewsletter(id,bestBefore);
+			}
+		if(idle)
+		{
+
+		TimeMe.callAfterTimeElapsedInSeconds(duration, function(){
+			popNewsletter(id,bestBefore);
+		});
+		}
+
+
+		});
+	
+});
 
 var scrollprogress = false;
 var scrollpercentage = 70;
@@ -13,21 +59,9 @@ var setCookie=true;
 
 
 
-var modalTrigger=false;
-function triggerModal() {
-	if(modalTrigger)
-	{
-		MicroModal.close('modal-1');
-		modalTrigger=false;
-	}
-	else{
-		MicroModal.show('modal-1');
-modalTrigger=true;
-		
-	}
-	
-	
-}
+
+
+
 
 function myFunction(e)
 {
@@ -42,6 +76,10 @@ jQuery('#newsletterpopup').remove();
 }, 1000);
 	
 }
+
+
+
+
 function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
@@ -58,13 +96,13 @@ function getCookie(cname) {
   return "";
 }
 
-function deleteCookie()
+function deleteCookie(id)
 {
-	document.cookie = "newsletter=true; expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=."+location.hostname+";path=/";
-	document.cookie = "newsletter=true; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+	document.cookie = "newsletter"+id+"=true; expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=."+location.hostname+";path=/";
+	document.cookie = "newsletter"+id+"=true; expires=Thu, 01 Jan 1970 00:00:00 GMT";
 }
 
-function popNewsletter() 
+function popNewsletter(id,bestBefore) 
 {
 	/*
 	var popup="<div class='popup' id='newsletterpopup' style='background-color: #ee7100; width: 300px; height:auto; min-height: 8%; display: block; position: fixed; top:46%; right:0; float: right;display: none;'><form id='newsletter' action='javascript:;' onsubmit='myFunction(this)'><p><input name='email' type='text' placeholder='E-Mail'></input></p><p><input name='else' type='text' placeholder='Noch irgendwas'></input></p><p><button type='submit' >Button</button></p></form></div>";
@@ -81,18 +119,33 @@ function popNewsletter()
 		//do sth....
 	}
 	*/
-	
-		if(!getCookie("newsletter")){
-		if(setCookie&&!getCookie("newsletter")){document.cookie = "newsletter=true;domain=."+location.hostname+";path=/";}
-		if(jQuery("#newsletterpopup").length == 0) {
+	if(bestBefore==0)
+	{
+		if(!getCookie("newsletter"+id)){
+		if(setCookie&&!getCookie("newsletter"+id)){document.cookie = "newsletter"+id+"=true;domain=."+location.hostname+";path=/";}
+
 		
 			
-			triggerModal();
+			triggerModal(id);
 		
 		}
 		
 		//do sth....
 	}
+	else
+		{
+		if(!getCookie("newsletter"+id)){
+			if(setCookie&&!getCookie("newsletter"+id)){
+				var today= new Date();
+				document.cookie = "newsletter"+id+"=true;domain=."+location.hostname+";path=/;expires="+today.setSeconds(today.getSeconds() + bestBefore);
+				}
+
+			
+				
+				triggerModal(id);
+			
+			}
+		}
 	
 	
 }
