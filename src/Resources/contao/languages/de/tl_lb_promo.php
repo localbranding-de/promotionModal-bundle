4135806c3870585152c0d178640d632d803b4fc0
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-05-20 14:17:43 +0200

    Update tl_lb_promo.php

--- src/Resources/contao/languages/de/tl_lb_promo.php
+++ src/Resources/contao/languages/de/tl_lb_promo.php
@@ -1,12 +1,16 @@
 <?php
 $GLOBALS['TL_LANG']['tl_lb_promo']['promo_legend'] = "Promotion-Aktionen";
+$GLOBALS['TL_LANG']['tl_lb_promo']['promoButton']  = "Buttoneinstellungen";
+$GLOBALS['TL_LANG']['tl_lb_promo']['promoNewsletterSpecial']  = "Newsletter Speziell";
+
 
 $GLOBALS['TL_LANG']['tl_lb_promo']['promoTitle']  = array('Titel' , 'Eindeutiger Name der Aktion.');
+$GLOBALS['TL_LANG']['tl_lb_promo']['newsletterFormAction']  = array(' Newsletter Form action' , '');
 $GLOBALS['TL_LANG']['tl_lb_promo']['promoDescription']  = array('Beschreibung' , 'Kurze Beschreibung der Promotion-Aktion.');
 $GLOBALS['TL_LANG']['tl_lb_promo']['promoTemplate']  = array('Aktionstyp / Template' , 'Template für den Aktionstyp auswählen.');
 $GLOBALS['TL_LANG']['tl_lb_promo']['headline']  = array('Headline' , 'Headline des Pop-Ups');
 $GLOBALS['TL_LANG']['tl_lb_promo']['bodytext']  = array('Body-Text' , 'Text des Pop-Ups');
-$GLOBALS['TL_LANG']['tl_lb_promo']['promoButton']  = array('Button' , 'Button (CTA) des Pop-Ups konfigurieren.');
+
 $GLOBALS['TL_LANG']['tl_lb_promo']['newsletterPrivacyConsent']  = array('Newsletter-Zustimmung' , 'NUR BEI NEWSLETTER: Einwilligungstext für Newsletter-Abo.');
 $GLOBALS['TL_LANG']['tl_lb_promo']['newsletterHint']  = array('Newsletter-Hinweis' , 'Hinweistext zum Newsletter-Abo (z. B. "Newsletter ist jederzeit kündbar".');
 $GLOBALS['TL_LANG']['tl_lb_promo']['published']  = array('Promo-Aktion veröffentlichen' , 'Die Promotion-Aktion auf der Webseite anzeigen.');
@@ -17,3 +21,8 @@ $GLOBALS['TL_LANG']['tl_lb_promo']['afterDurationSec']  = array('Besuchsdauer' ,
 $GLOBALS['TL_LANG']['tl_lb_promo']['bestBefore']  = array('Verfallszeit (Sek)' , 'Zeit die vergehen muß, bis dasselbe Promo-Popup demselben Besucher erneut gezeigt wird. In Sekunden z.B. 86400 = 1 Tag');
 $GLOBALS['TL_LANG']['tl_lb_promo']['start']  = array('Start' , 'Start der Akion. Feld leer lassen, wenn die Aktion dauerhaft laufen soll.');
 $GLOBALS['TL_LANG']['tl_lb_promo']['stop']  = array('Stopp' , 'Ende der Akion. Feld leer lassen, wenn die Aktion dauerhaft laufen soll.');
+$GLOBALS['TL_LANG']['tl_lb_promo']['buttonUrlBlog']  = array('Blog URL' , 'Interne Url zu Blogbeitrag');
+$GLOBALS['TL_LANG']['tl_lb_promo']['buttonUrlExt']  = array('externe URL' , 'Externe Url ');
+$GLOBALS['TL_LANG']['tl_lb_promo']['buttonProductDetail']  = array('Productdetailseite' , 'Das Produkt auswählen für die Weiterleitung.');
+$GLOBALS['TL_LANG']['tl_lb_promo']['buttonPageUrl']  = array('Seiten Url' , 'Seite auswählen für die Weiterleitung.');
+$GLOBALS['TL_LANG']['tl_lb_promo']['onPages']  = array('Seitenauswahl' , 'Auf welchen Seiten soll die Aktion aktiv sein');
